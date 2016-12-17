99b4abaa52639fc4c13cd099d4438b186b5ffd40
vagrant <vagrant@home-centos6.(none)>
2016-12-17 15:53:14 +0000

    add 5 tutorials

---
+++ tutorial.4/hellomake.c
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hellomake.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  12/17/2016 01:07:05 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Wolfgang Mehner (wm), wolfgang-mehner@web.de
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <hellomake.h>
+
+int main()
+{
+	// call a function in another file
+	myPrintHelloMake();
+
+	return 0;
+}
